5a261d3b582b41a5c7527b390dfa78e763cd00e5
Alexandre Coudray <alexandre.coudray@epfl.ch>
2023-07-28 17:58:09 +0200

    fixed bug in early stopping

--- R/cliff.R
+++ R/cliff.R
@@ -172,13 +172,13 @@ cliff <- function (climb_output, drug_data, mutation_data = NULL, min.mutation =
         rmse_ = rmse(rowSums(as.matrix(PI_hat_nk) * as.matrix(climb_prop)), 
             auc_values)
         print(abs(rmse_ - min_rmse))
-        if ( e > 1 & ( abs(rmse_ - min_rmse) < 1e-4 | rmse_ < min_rmse ){
+        if ( e > 1 & rmse_ < min_rmse ){
             min_rmse = rmse_
             min_coefs = cliff_coefs
             min_PI = PI_hat_nk
             it.increasing.rmse = 0
         }
-        else if (abs(rmse_ - min_rmse) < 1e-4 | rmse_ > min_rmse ){
+        else if (abs(rmse_ - min_rmse) < 1e-4  | rmse_ > min_rmse ){
             it.increasing.rmse = it.increasing.rmse + 1
             if (it.increasing.rmse >= 3) {
                 message(paste0("early stopping of EM algorithm at step ", 
